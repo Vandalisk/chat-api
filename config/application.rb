f7d555aa9ceb8af30dac76be00db8636ec349dfd
ILya <ilya.zakharov@mechanizm.io>
2016-06-11 15:43:30 +0300

    added rspec for testing

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module ChatApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators.test_framework  :rspec, :fixture => false
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
