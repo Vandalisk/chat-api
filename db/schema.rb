84fa822e222adf6e46676c126ed7b29c659db328
ILya <ilya.zakharov@mechanizm.io>
2016-06-12 13:07:24 +0300

    added users and messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160612095329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sender_id",                null: false
+    t.integer  "receiver_id",              null: false
+    t.text     "body",        default: "", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id", using: :btree
+    t.index ["sender_id"], name: "index_messages_on_sender_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      default: "", null: false
+    t.string   "first_name",              null: false
+    t.string   "last_name",               null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+  end
 
 end
